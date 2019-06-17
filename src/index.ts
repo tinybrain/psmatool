7d6701af685b2e7c21c0fe844b43cf5e386e57ad
Simon Lang <simon@tinybra.in>
2019-06-18 01:41:11 +1000

    vscode tasks etc

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --harmony
+// require('source-map-support').install();
 
 import { Command } from 'commander';
 const command = new Command()
 
 // const program = require('commander');
+
 
 command
   .version('dev')
@@ -16,6 +18,12 @@ command
   .action(() => {
     console.log('oooh!')
   })
+
+let dummy: any = {};
+
+console.log(dummy.not_here);
+
+dummy.barry()
 
 if(!process.argv.slice(2).length/* || !/[arudl]/.test(process.argv.slice(2))*/) {
     command.outputHelp()
